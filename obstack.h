ffdce4dd72578cd6649658629d291feaaa6a3d94
PartyPlanner64 <partyplanner64@users.noreply.github.com>
2019-07-24 20:01:19 -0500

    Patch obstack.h

--- obstack.h
+++ obstack.h
@@ -331,14 +331,14 @@ __extension__								\
 /* These assume that the obstack alignment is good enough for pointers or ints,
    and that the data added so far to the current object
    shares that much alignment.  */
-   
-#define obstack_ptr_grow(OBSTACK,datum)					\
+
+# define obstack_ptr_grow(OBSTACK,datum)					\
 __extension__								\
 ({ struct obstack *__o = (OBSTACK);					\
    if (__o->next_free + sizeof (void *) > __o->chunk_limit)		\
      _obstack_newchunk (__o, sizeof (void *));				\
-   if (!__o->alloc_failed)						\
-     *((void **)__o->next_free)++ = ((void *)datum);			\
+   *((void **)__o->next_free) = ((void *)datum);						\
+   __o->next_free += sizeof (void *);			\
    (void) 0; })
 
 #define obstack_int_grow(OBSTACK,datum)					\
